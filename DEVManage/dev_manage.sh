5d2696d675c0ac83d336d4c00a4a3cca8f0ce5a0
Dmitriy Goncharenko <dimargfd@gmail.com>
2019-06-19 10:17:13 +0300

    update dev_manage

--- DEVManage/dev_manage.sh
+++ DEVManage/dev_manage.sh
@@ -6,23 +6,41 @@
 # 
 # Usage: bash dev_manage.sh [mule|sas|mq|fed|dev] [stop|start|status|restart]
 # 
-# GlowByte, v2.0, 03.2019
+# GlowByte, v2.1, 06.2019
 #
 
 # Print help
 if  [ $# -eq 0 ] || [ $1 == '-h' ] || [ $1 == "-help" ] || [ $1 == "--help" ]; then
     echo
-    echo "-------------------- INFO --------------------"
-    echo "Usage: bash dev_manage.sh [-h] [SYSTEM [SUB]] [NO] ACTION"
+    echo "--------------------- INFO - HELP ---------------------"
+    echo "Usage: bash dev_manage.sh [-h] [SYSTEM [SUB]] ACTION"
     echo "------                                        "
     echo "-h,--help   - prints this help message        "
     echo "SYSTEM      - mule | sas | mq | fed | dev     "
     echo "              if SYSTEM not specified - dev   "
     echo "SUB         - subsystem for SAS management    "
-    echo "            - if SUB not specified - all SAS  " # TODO: add subsystems for sas
-    echo "NO          - sas or fed server number        "
+    echo "            - if SUB not specified - all SAS  "
     echo "ACTION      - stop | start | status | restart "
-    echo "----------------------------------------------"
+    echo "-------------------------------------------------------"
+    echo "Use: bash dev_manage.sh sas [-h] for info about servers"
+    echo "-------------------------------------------------------"
+    echo
+    exit 0
+fi
+
+# Print SAS Servers help
+if [ $# -gt 1 ] && [ $1 == "sas" ] && ([ $2 == '-h' ] || [ $2 == "-help" ] || [ $2 == "--help" ]); then
+    echo
+    echo "--------------------- INFO - SAS SERVERS ---------------------"
+    echo "Usage: bash dev_manage.sh sas [-h] [1|2|6|7]  ACTION"
+    echo "------                                        "
+    echo "-h,--help   - prints this help message        "
+    echo "ACTION      - stop | start | status | restart "
+    echo "-------------------- AVAILABLE SUBSYSTEMS --------------------"
+    echo "1,2   - SAS Server 1,2    (SAS Content Server, Metadata Server, Remote Services)"
+    echo "6     - SAS Server 6      (SAS CI Studio)"
+    echo "7     - SAS Server 7      (SAS Design Server, SAS Engine Server)"
+    echo "-------------------------------------------------------"
     echo
     exit 0
 fi
@@ -31,14 +49,14 @@ sys_n=""
 # Parse arguments
 if [ $# -lt 2 ]; then
     sys="dev"
-    arg=$1
+    arg="$1"
 elif [ $# -eq 2 ]; then
-    sys=$1
-    arg=$2
+    sys="$1"
+    arg="$2"
 elif [ $# -eq 3 ]; then
-    sys=$1
-    sys_n=$2
-    arg=$3
+    sys="$1"
+    sys_n="$2"
+    arg="$3"
 else
     echo "To many arguments: $#"
     echo "Use -h for help"
@@ -46,45 +64,51 @@ else
 fi
 
 # Checking arguments
-if [ $arg == "start" ] || [ $arg == "stop" ] || [ $arg == "restart" ] || [ $arg == "status" ]; then
+if [ "$arg" == "start" ] || [ "$arg" == "stop" ] || [ "$arg" == "restart" ] || [ "$arg" == "status" ]; then
     if [ $sys == "dev" ]; then
       
-        if [ $arg != "status" ]; then
+        if [ "$arg" != "status" ]; then
             read -p "Press ENTER to $arg DEV..."
         fi
     
-    elif [ $# -ne 3 ] && ([ $sys == "mule" ] || [ $sys == "mq" ]); then
+    elif [ $# -ne 3 ] && ([ "$sys" == "mule" ] || [ "$sys" == "mq" ] || [ "$sys" == "fed" ]); then
 
         if [ $arg != "status" ]; then
             read -p "Press ENTER to $arg $sys..."
         fi
 
-    elif [ $sys == "sas" ] || [ $sys == "fed" ]; then
+    elif [ "$sys" == "sas" ]; then
         
         if [ $# -eq 3 ]; then
             
-            if [ $sys == "sas" ]; then
-                case "$sys_n" in 
-                1|2|6|7|11)
-                    ;;
-                *) 
-                    echo "Error: No Sas Server $sys_n found"
-                    echo "Use -h for help"
-                    exit 1
-                esac
-            else # fed
-                case "$sys_n" in 
-                1|2)
-                    ;;
-                *) 
-                    echo "Error: No Fed Server $sys_n found"
-                    echo "Use -h for help"
-                    exit 1
-                esac
-            fi
+            case "$sys_n" in 
+            1|2)
+                sys_n=""
+                sysname="ALL SAS SERVERS"
+                saspath=/sas/sasconfig/Lev1/sas.servers
+            ;;
+            6)
+                sysname="SAS Server 6 (CI Studio)"
+                saspath=/sas/sasconfig/Lev1/Web/WebAppServer/SASServer6_1/bin/tcruntime-ctl.sh
+            ;;
+            7)
+                sysname="SAS Server 7 (Design & Engine Servers)"
+                saspath=/sas/sasconfig/Lev1/Web/WebAppServer/SASServer6_1/bin/tcruntime-ctl.sh
+            ;;
+            12|13)
+                echo "WARNING: <anage option for SAS Server $sys_n is in developing"
+                echo "Use -h for help"
+                exit 1
+            ;;
+            *) 
+                echo "Error: No Sas Server $sys_n found"
+                echo "Use -h for help"
+                exit 1
+            esac
+
         fi   
         
-        if [ $arg != "status" ]; then
+        if [ "$arg" != "status" ]; then
             read -p "Press ENTER to $arg $sys $sys_n..."
         fi
         
@@ -100,147 +124,38 @@ else
 fi
 
 time_start=$SECONDS
-case "$arg" in
-start)
-    if [ $sys = "dev" ] || [ $sys = "sas" ]; then
+if [ "$sys" = "dev" ] || [ "$sys" = "sas" ]; then
+    if [ -z "$sys_n" ]; then
         echo "-------------------- INFO --------------------"
-        echo "               SAS.SERVERS start              "
+        echo "               ALL SAS SERVERS ${arg}         "
         echo "----------------------------------------------"
-        /sas/sasconfig/Lev1/sas.servers start
+        /sas/sasconfig/Lev1/sas.servers ${arg}
+    else
+        echo "-------------------- INFO --------------------"
+        echo "               ${sysname} ${arg}              "
+        echo "----------------------------------------------"
+        ${syspath} ${arg}
     fi
-    if [ $sys = "dev" ] || [ $sys = "fed" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "              FED Servers start              "
-        echo "----------------------------------------------"
-        /sas/sashome/SASFederationServer/4.1/fedserver/bin/dfsadmin start
-        /sas/sashome/SASFederationServer2/4.1/fedserver/bin/dfsadmin start
-    fi
-    if [ $sys = "dev" ] || [ $sys = "mule" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                  Mule starts                 "
-        echo "----------------------------------------------"
-        /sas/mule-standalone/bin/mule start
-    fi
-    if [ $sys = "dev" ] || [ $sys = "mq" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                ActiveMQ starts               "
-        echo "----------------------------------------------"
-        /sas/apache-activemq-5.15.2/bin/activemq start
-    fi
-    ;;
-stop)
-    if [ $sys = "dev" ] || [ $sys = "mq" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                ActiveMQ stops                "
-        echo "----------------------------------------------"
-        /sas/apache-activemq-5.15.2/bin/activemq stop
-    fi
-    if [ $sys = "dev" ] || [ $sys = "mule" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                  Mule stops                  "
-        echo "----------------------------------------------"
-        /sas/mule-standalone/bin/mule stop
-    fi
-    if [ $sys = "dev" ] || [ $sys = "fed" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "               FED Servers stop               "
-        echo "----------------------------------------------"
-        /sas/sashome/SASFederationServer/4.1/fedserver/bin/dfsadmin stop
-        /sas/sashome/SASFederationServer2/4.1/fedserver/bin/dfsadmin stop
-    fi
-    if [ $sys = "dev" ] || [ $sys = "sas" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                SAS.SERVERS stop              "
-        echo "----------------------------------------------"
-        /sas/sasconfig/Lev1/sas.servers stop
-    fi
-    ;;
-status)
-    if [ $sys = "dev" ] || [ $sys = "sas" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "              SAS.SERVERS status              "
-        echo "----------------------------------------------"
-        /sas/sasconfig/Lev1/sas.servers status
-    fi
-    if [ $sys = "dev" ] || [ $sys = "mule" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                  Mule status                 "
-        echo "----------------------------------------------"
-        /sas/mule-standalone/bin/mule status
-    fi
-    if [ $sys = "dev" ] || [ $sys = "fed" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "             FED Server 1 status              "
-        echo "----------------------------------------------"
-        /sas/sashome/SASFederationServer/4.1/fedserver/bin/dfsadmin status
-        echo "-------------------- INFO --------------------"
-        echo "             FED Server 2 status              "
-        echo "----------------------------------------------"
-        /sas/sashome/SASFederationServer2/4.1/fedserver/bin/dfsadmin status
-    fi
-    if [ $sys = "dev" ] || [ $sys = "mq" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                ActiveMQ status               "
-        echo "----------------------------------------------"
-        /sas/apache-activemq-5.15.2/bin/activemq status
-    fi
-    ;;
-restart)
-    if [ $sys = "dev" ] || [ $sys = "mq" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                 ActiveMQ stops               "
-        echo "----------------------------------------------"
-        /sas/apache-activemq-5.15.2/bin/activemq stop
-    fi
-    if [ $sys = "dev" ] || [ $sys = "mule" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                  Mule stops                  "
-        echo "----------------------------------------------"
-        /sas/mule-standalone/bin/mule stop
-    fi
-    if [ $sys = "dev" ] || [ $sys = "fed" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "               FED Servers stop               "
-        echo "----------------------------------------------"
-        /sas/sashome/SASFederationServer/4.1/fedserver/bin/dfsadmin stop
-        /sas/sashome/SASFederationServer2/4.1/fedserver/bin/dfsadmin stop
-    fi
-    if [ $sys = "dev" ] || [ $sys = "sas" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                SAS.SERVERS stop              "
-        echo "----------------------------------------------"
-        /sas/sasconfig/Lev1/sas.servers stop
-        echo "-------------------- INFO --------------------"
-        echo "               SAS.SERVERS start              "
-        echo "----------------------------------------------"
-        /sas/sasconfig/Lev1/sas.servers start
-    fi
-    if [ $sys = "dev" ] || [ $sys = "fed" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "               FED Servers start              "
-        echo "----------------------------------------------"
-        /sas/sashome/SASFederationServer/4.1/fedserver/bin/dfsadmin start
-        /sas/sashome/SASFederationServer2/4.1/fedserver/bin/dfsadmin start
-    fi
-    if [ $sys = "dev" ] || [ $sys = "mule" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                  Mule starts                 "
-        echo "----------------------------------------------"
-        /sas/mule-standalone/bin/mule start
-    fi
-    if [ $sys = "dev" ] || [ $sys = "mq" ]; then
-        echo "-------------------- INFO --------------------"
-        echo "                ActiveMQ starts               "
-        echo "----------------------------------------------"
-        /sas/apache-activemq-5.15.2/bin/activemq start
-    fi
-    ;;
-    *)
-    echo "ERROR: No [$arg] action found"
-    echo "Use -h for help"
-    exit 1
-    ;;
-esac
+fi
+if [ "$sys" = "dev" ] || [ "$sys" = "fed" ]; then
+    echo "-------------------- INFO --------------------"
+    echo "              FED Servers ${arg}              "
+    echo "----------------------------------------------"
+    /sas/sashome/SASFederationServer/4.1/fedserver/bin/dfsadmin ${arg}
+fi
+if [ "$sys" = "dev" ] || [ "$sys" = "mule" ]; then
+  echo "-------------------- INFO --------------------"
+echo "                  Mule ${arg}                 "
+echo "----------------------------------------------"
+/sas/mule-standalone/bin/mule ${arg}
+fi
+
+if [ "$sys" = "dev" ] || [ "$sys" = "mq" ]; then
+    echo "-------------------- INFO --------------------"
+    echo "                ActiveMQ ${arg}               "
+    echo "----------------------------------------------"
+    /sas/apache-activemq-5.15.2/bin/activemq ${arg}
+fi
 
 timer=$(( $SECONDS - time_start ))
 min=$((timer / 60))
